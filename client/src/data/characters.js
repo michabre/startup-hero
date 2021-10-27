2b543e8bb84d621abbc900dc005d212c0a72e096
Michael Breuer <michael.breuer@aspirecreative.co.uk>
2021-10-27 10:25:47 +0800

    character designs added

--- client/src/data/characters.js
+++ client/src/data/characters.js
@@ -1,24 +1,26 @@
 const Artist = [
   {
     name: "Classical Artist",
-    description: "Their website design could be displayed next to the Mona Lisa.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    description:
+      "Their website design could be displayed next to the Mona Lisa.",
+    clothing: "classical-artist_clothing.png",
+    face: "classical-artist_face.png",
+    hair: "classical-artist_hair.png",
   },
   {
     name: "Web Designer",
     description: "Clients never stop talking about how much their designs pop.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    clothing: "web-designer_clothing.png",
+    face: "web-designer_face.png",
+    hair: "web-designer_hair.png",
   },
   {
     name: "Technical Designer",
-    description: "Perfect grid systems. The mathmeticians of ancient Greece would be proud.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    description:
+      "Perfect grid systems. The mathmeticians of ancient Greece would be proud.",
+    clothing: "technical-artist_clothing.png",
+    face: "technical-artist_face.png",
+    hair: "technical-artist_hair.png",
   },
 ];
 
@@ -26,23 +28,25 @@ const Hacker = [
   {
     name: "First Class Nerd",
     description: "20 lines of code from them could already get you to an IPO.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    clothing: "old-school-nerd_clothing.png",
+    face: "old-school-nerd_face.png",
+    hair: "old-school-nerd_hair.png",
   },
   {
     name: "Geek Chic",
-    description: "They write answers on Stack Overflow that get as much positive comments as their tutorial videos.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    description:
+      "They write answers on Stack Overflow that get as much positive comments as their tutorial videos.",
+    clothing: "web-developer_clothing.png",
+    face: "web-developer_face.png",
+    hair: "web-developer_hair.png",
   },
   {
-    name: "L33T",
-    description: "After completing another bug bounty to avoid economic collapse, they continue looking for Morpheus.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    name: "L337",
+    description:
+      "After completing another bug bounty to avoid economic collapse, they continue looking for Morpheus.",
+    clothing: "h4x0r_clothing.png",
+    face: "h4x0r_face.png",
+    hair: "h4x0r_hair.png",
   },
 ];
 
@@ -50,23 +54,23 @@ const Hustler = [
   {
     name: "Rockstar Salesman",
     description: "They could sell cloud services to Amazon.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    clothing: "salesman_clothing.png",
+    face: "salesman_face.png",
+    hair: "salesman_hair.png",
   },
   {
     name: "Project Manager",
     description: "Their projects run like clockwork.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    clothing: "project-manager_clothing.png",
+    face: "project-manager_face.png",
+    hair: "project-manager_hair.png",
   },
   {
     name: "Visionary",
     description: "Always ahead of the game.",
-    clothing: "",
-    face: "",
-    accessories: "",
+    clothing: "visionary_clothing.png",
+    face: "visionary_face.png",
+    hair: "visionary_hair.png",
   },
 ];
 
